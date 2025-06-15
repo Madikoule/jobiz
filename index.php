bca73280649b8b79ccad3a7a336edeb8235e2251
youssouf doucoure <madkdouc@yahoo.com>
2025-06-15 11:50:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pole emploi</title>
+</head>
+
+<div class="title">
+    <h1>Pole Service Bamako</h1>
+</div>
+
+<body>
+    
+</body>
+</html>
